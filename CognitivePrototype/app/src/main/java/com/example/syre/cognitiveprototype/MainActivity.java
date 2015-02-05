f0ef2646b8fe771d920a07dcd5665925590cf30d
Søren Howe Gersager <syrelyre@gmail.com>
2015-02-05 16:34:45 +0100

    added initial prototype

--- CognitivePrototype/app/src/main/java/com/example/syre/cognitiveprototype/MainActivity.java
+++ CognitivePrototype/app/src/main/java/com/example/syre/cognitiveprototype/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.syre.cognitiveprototype;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button)findViewById(R.id.show_numbers_start_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ShowSuccessionNumbersStart.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
