6749289025acd812225cb95c04d4f20eedb4a606
Søren Howe Gersager <syrelyre@gmail.com>
2015-04-07 15:38:39 +0200

    updated UI, added broadcast button

---
+++ project/FriendBump/mobile/src/main/java/com/example/syre/friendbump/MainActivity.java
@@ -0,0 +1,163 @@
+package com.example.syre.friendbump;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity implements OnMapReadyCallback, LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    ArrayList<String> listItems = new ArrayList<String>();
+    MapView friendmapview;
+    GoogleApiClient mGoogleApiClient;
+    Boolean broadcastingEnabled;
+    ImageButton toggleBroadcastingButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        friendmapview = (MapView)findViewById(R.id.friendMapView);
+        toggleBroadcastingButton = (ImageButton)findViewById(R.id.toggleBroadcastingButton);
+        friendmapview.onCreate(savedInstanceState);
+
+        ListView friendListView = (ListView)findViewById(R.id.friendListView);
+        listItems.add("Søren Howe Gersager");
+        listItems.add("Anders Rahbek");
+        friendListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems));
+
+        MapsInitializer.initialize(this);
+        friendmapview.getMapAsync(this);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                            .addConnectionCallbacks(this)
+                            .addOnConnectionFailedListener(this)
+                            .addApi(LocationServices.API)
+                            .build();
+        broadcastingEnabled = true;
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        friendmapview.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        friendmapview.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        friendmapview.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        friendmapview.onLowMemory();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        // gets last known location
+        LocationManager locationmanager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = locationmanager.getBestProvider(criteria, false);
+        Location last_known = locationmanager.getLastKnownLocation(provider);
+
+        CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(last_known.getLatitude(), last_known.getLongitude()));
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
+        // enables location marker
+        map.setMyLocationEnabled(true);
+        map.getUiSettings().setAllGesturesEnabled(false);
+        map.moveCamera(center);
+        map.animateCamera(zoom);
+
+    }
+    @Override
+    public void onLocationChanged(Location loc)
+    {
+        if (broadcastingEnabled)
+        {
+            // broadcast location
+        }
+
+    }
+    @Override public void onConnectionSuspended(int id)
+    {
+
+    }
+    @Override public void onConnected(Bundle bundle)
+    {
+        LocationRequest locationrequest = new LocationRequest();
+        locationrequest.setInterval(10000);
+        locationrequest.setFastestInterval(5000);
+        locationrequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,locationrequest, this);
+        Toast.makeText(this, "Connection successful", Toast.LENGTH_LONG).show();
+    }
+
+    @Override public void onConnectionFailed(ConnectionResult result)
+    {
+        Toast.makeText(this, "Connection failed", Toast.LENGTH_LONG).show();
+    }
+    public void toggleBroadcasting(View v)
+    {
+        if (broadcastingEnabled)
+        {
+            toggleBroadcastingButton.setBackgroundResource(R.mipmap.enable_broadcasting);
+        }
+        else
+        {
+            toggleBroadcastingButton.setBackgroundResource(R.mipmap.disable_broadcasting);
+        }
+        broadcastingEnabled = !broadcastingEnabled;
+    }
+}
---
+++ project/FriendBump/wear/src/main/java/com/example/syre/friendbump/FriendNotificationActivity.java
@@ -0,0 +1,24 @@
+package com.example.syre.friendbump;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class FriendNotificationActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_notification);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
