dcc279a45537bcfc1403b3cdad3aaa4d94a7b698
Søren Howe Gersager <syrelyre@gmail.com>
2015-04-09 17:18:59 +0200

    added test publisher and subscriber

---
+++ project/test_mqtt_publisher.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import paho.mqtt.client as paho
+import json
+
+def on_connect(client, userdata, flags, rc):
+	print("Connected with result code "+str(rc))
+	print(client.publish("amq.topic", "hello"))
+
+def on_disconnect(client, userdata, flags, rc):
+	if rc != 0:
+		print("Unexpected disconnection.")
+def on_message(client, userdata, msg):
+	print(msg.payload)
+
+client = paho.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_disconnect = on_disconnect
+client.connect("syrelyre.dk", 1883, 60)
+msg = """{id:client501, command: loc_update, lat: 55.706, lng: 12.536}"""
+client.publish("friendbump", payload=msg)
---
+++ project/test_mqtt_subscriber.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import paho.mqtt.client as paho
+import json
+
+def on_connect(client, userdata, flags, rc):
+	print("Connected with result code "+str(rc))
+
+def on_disconnect(client, userdata, flags, rc):
+	if rc != 0:
+		print("Unexpected disconnection.")
+def on_message(client, userdata, msg):
+	print(msg.payload)
+
+client = paho.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_disconnect = on_disconnect
+client.connect("syrelyre.dk", 1883, 60)
+client.subscribe("friendbump",2)
+while client.loop() == 0:
+    pass
+
