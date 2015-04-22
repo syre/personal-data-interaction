b64443449f023e635a37fa362e8e86e50af0bbf3
Søren Howe Gersager <syrelyre@gmail.com>
2015-04-22 16:34:59 +0200

    now broadcasting leave message on last location

--- project/FriendBump/mobile/src/main/java/com/example/syre/friendbump/MainActivity.java
+++ project/FriendBump/mobile/src/main/java/com/example/syre/friendbump/MainActivity.java
@@ -317,15 +317,16 @@ public class MainActivity extends Activity implements OnMapReadyCallback,
         CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
         mMap.animateCamera(zoom);
         subscribeToFriends(loc);
+        if (lastLocation == null)
+            lastLocation = loc;
         Log.d("MainActivity","Location changed to: lat: "+loc.getLatitude()+", lng: "+loc.getLongitude());
         if (broadcastingEnabled)
         {   // if location when converted to accuracy of 110m (3 decimal places) has changed
-            if (lastLocation == null || roundtoDecimals(3, loc.getLatitude()) != roundtoDecimals(3, lastLocation.getLatitude()) ||
+            if (roundtoDecimals(3, loc.getLatitude()) != roundtoDecimals(3, lastLocation.getLatitude()) ||
                     roundtoDecimals(3, loc.getLongitude()) != roundtoDecimals(3, lastLocation.getLongitude()))
             {
                 sendNewAreaUpdate(lastLocation);
-                if (lastLocation != null)
-                    unsubscribeToFriends(lastLocation);
+                unsubscribeToFriends(lastLocation);
                 subscribeToFriends(loc);
                 Log.d("MainActivity", "Sending new area update (loc_remove)");
                 Toast.makeText(getApplicationContext(), "Sending new area update (loc_remove)", Toast.LENGTH_LONG).show();
