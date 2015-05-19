adb162b77e81991ff79f60d7bbdfad1f0fb7e70b
Søren Howe Gersager <syrelyre@gmail.com>
2015-05-20 00:21:29 +0200

    added blue on arrow

--- project/FriendBump/mobile/src/main/java/com/example/syre/friendbump/FriendListAdapter.java
+++ project/FriendBump/mobile/src/main/java/com/example/syre/friendbump/FriendListAdapter.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.net.Uri;
 import android.provider.ContactsContract;
 import android.util.Log;
